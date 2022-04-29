d969d9dd73acf519f21150914e92464b7ec325f2
Rashika1998 <rashikamr1998@gmail.com>
2022-04-29 20:35:22 +0530

    Product functionality updated.

--- ProductServicePublisher/src/productservicepublisher/ProductServicePublishImpl.java
+++ ProductServicePublisher/src/productservicepublisher/ProductServicePublishImpl.java
@@ -1,8 +1,9 @@
 package productservicepublisher;
 
+import java.util.Scanner;
 
 public class ProductServicePublishImpl implements ProductServicePublish {
-
+	
 	Item item_1 = new Item(1, "Monitor", "ITEM_001", "Monitor description", 25.0);
 	Item item_2 = new Item(2, "Key Board", "ITEM_002", "Key Board description", 15.0);
 	Item item_3 = new Item(3, "Mouse", "ITEM_003", "Mouse description", 12.0);
@@ -22,6 +23,38 @@ public class ProductServicePublishImpl implements ProductServicePublish {
 		
 		System.out.println();
 		System.out.println("--------------------------------------------");
+		System.out.println();
+		System.out.println();
+		System.out.println("Add new product....");
+		
+		Scanner scanner = new Scanner(System.in);
+		Item item_6 = new Item();
+		
+		System.out.println("Enter product Id:");
+		String id = scanner.nextLine();
+		
+		System.out.println("Enter product name:");
+		String name = scanner.nextLine();
+		
+		System.out.println("Enter product price:");
+		String price = scanner.nextLine();
+		
+		System.out.println("Enter product code:");
+		String code = scanner.nextLine();
+		
+		System.out.println("Enter product desc:");
+		String desc = scanner.nextLine();
+		
+		int pid = Integer.parseInt(id);
+		double pprice = Double.parseDouble(price);
+		
+		item_6.setId(pid);
+		item_6.setItemCode(code);
+		item_6.setItemName(name);
+		item_6.setItemDesc(desc);
+		item_6.setItemPrice(pprice);
+		
+		System.out.println(item_6.toStringWith());
 		
 		return "Done";
 		
